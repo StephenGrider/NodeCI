565f78591dc4b465330439b14e22fb1e14dec2c5
stephengrider <ste.grider@gmail.com>
2018-02-27 11:57:59 -0800

    headless

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -5,7 +5,7 @@ const userFactory = require('../factories/userFactory');
 class CustomPage {
   static async build() {
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       args: ['--no-sandbox']
     });
 
