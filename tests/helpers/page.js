2c2317154a5d32021badb316bc0e83c16f1b9f04
stephengrider <ste.grider@gmail.com>
2018-02-27 11:31:19 -0800

    Added chrome launch options

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -5,7 +5,8 @@ const userFactory = require('../factories/userFactory');
 class CustomPage {
   static async build() {
     const browser = await puppeteer.launch({
-      headless: false
+      headless: true,
+      args: ['--no-sandbox']
     });
 
     const page = await browser.newPage();
